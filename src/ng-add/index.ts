4edf42d38a2e94c02ccfc99a7167f23197050513
Firdaus <mail4frenz@gmail.com>
2024-05-20 07:24:55 +0800

    feat: Add ng-add and ng-update-webpack functions
    The `ng-add` function is an exported helper function in TypeScript that creates a new rule. It takes a schema object as a parameter and returns a newly created rule that performs a series of operations, including preparing the workspace, creating a new folder path, moving and transforming a set of files, and applying a chain of other rules like installing dependencies, adding module federated, and updating the Webpack configuration. The combination of these operations forms a chained rule, which is returned by the `add` function.
    
    The `ng-update-webpack` function returns a rule that updates the Webpack configuration of an Angular project based on the provided options. It reads the workspace configuration from the `angular.json` file and uses Angular Devkit Schematics for creating, modifying, or deleting files in the workspace.
    
    The commit also includes utility functions such as `specFilter`, `getWorkspace`, `installModuleFederated`, `addModuleFederated`, `updateWebpackConfig`, `installDependencyPackage`, and `getAsSourceFile`, that are used within the `ng-add` and `ng-update-webpack` functions to perform specific tasks.
    
    Note: The code changes in the diff include adding JSDoc comments to document the functions and adding new functions with their respective implementations.

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -19,6 +19,17 @@ import * as ts from "typescript";
 let taskId: TaskId;
 let webpackTaskId: TaskId;
 
+/**
+ * The `add` function is an exported helper function in TypeScript that creates a new rule.
+ * 
+ * @param {Schema} _options - A schema object that contains the specifications for the rule.
+ * 
+ * @return {Rule} The function returns a newly created rule that performs a series of operations, 
+ * including preparing the workspace, creating a new folder path, moving and transforming a set of files, 
+ * and applying a chain of other rules like installing dependencies, adding module federated and 
+ * updating the Webpack configuration. The combination of these operations forms a chained rule, 
+ * which is returned by the `add` function.
+ */
 export function add(_options: Schema): Rule {
     return (tree: Tree, _context: SchematicContext) => {
         const workspace = getWorkspace(_options, tree);
@@ -56,6 +67,21 @@ export function add(_options: Schema): Rule {
     };
 }
 
+/**
+ * `specFilter` is a function that returns a rule to filter test related files in the project,
+ * based on the `spec` option.
+ * 
+ * It takes an object of the type `Schema` as a parameter, which includes the property `spec`.
+ * When `spec` is falsy, the function filters out both `.spec.ts` files and `test.ts` files. 
+ * If `spec` is truthy, only `test.ts` files are filtered.
+ * The function uses regex to match the file paths and filter the required files.
+ * 
+ * This function makes use of the `filter` function from the '@angular-devkit/schematics' package
+ * to create a rule which can be used in a chain of schematic rules.
+ * 
+ * @param _options - The options to configure how the filtering will be done.
+ * @returns - A rule for filtering the `.spec.ts` and `test.ts` files based on the `_options.spec` value.
+ */
 function specFilter(_options: Schema): Rule {
     if (!_options.spec) {
         return filter(path => {
@@ -66,14 +92,37 @@ function specFilter(_options: Schema): Rule {
     return filter(path => !path.match(/test\.ts$/));
 }
 
+/**
+ * getWorkspace is a function that reads the 'angular.json' file from the tree and returns its content as a JavaScript
+ * object parsed from the original JSON format.
+ * 
+ * @param {_options} - An object of the Schema type. Not used inside this function but potentially relevant for function calls.
+ * @param {tree} - An object of the Tree type from the '@angular-devkit/schematics' package. Represents the current file 
+ * system tree, which is modified by the schematics.
+ *
+ * @throws {SchematicsException} - If the 'angular.json' file does not exist in the current tree, the function will throw 
+ * a SchematicsException with the message 'angular.json file not found'.
+ *
+ * @returns {workspaces.WorkspaceDefinition} - This function returns an object containing workspace definitions read
+ * from the 'angular.json' file. The return type is WorkspaceDefinition from the '@angular-devkit/core' package.
+ */
 function getWorkspace(_options: Schema, tree: Tree): workspaces.WorkspaceDefinition {
     const workspace = tree.read('./angular.json');
-    if(!workspace) {
+    if (!workspace) {
         throw new SchematicsException('angular.json file not found');
     }
     return JSON.parse(workspace.toString());
 }
 
+/**
+ * The `installModuleFederated` function creates a rule that installs the '@angular-architects/module-federation' package.
+ * The installation is achieved by creating and adding a new task to the context. The task specifically installs 
+ * a package with the name '@angular-architects/module-federation'.
+ * 
+ * This function does not expect any parameters and returns a Rule from the '@angular-devkit/schematics' package.
+ * The returned rule is a function that takes a Tree and a SchematicContext as parameters and installs the required 
+ * package, then returns the modified tree.
+ */
 function installModuleFederated(): Rule {
     return (tree: Tree, _context: SchematicContext) => {
         const mfDepName = '@angular-architects/module-federation';
@@ -87,6 +136,28 @@ function installModuleFederated(): Rule {
     }
 }
 
+/**
+ * The `addModuleFederated` function constructs and applies a rule to augment the configuration of a current Angular project
+ * based on the given schema `options`. 
+ *
+ * @param {Schema} _options - The object containing the specifications for the rule from the `addModuleFederated` function. 
+ * The `options` parameter should contain three properties: `type`, `project`, and `port`.
+ *
+ * @returns {Rule} - Returns an Angular Schematics `Rule` function that takes a `Tree` and a `SchematicContext` as parameters,
+ * and modifies the context by adding a new task `RunSchematicTask` based on the extracted `_options` provided,
+ * finally returning the modified tree.
+ *
+ * Specifically, the `RunSchematicTask` will run the `ng-add` schematic from the '@angular-architects/module-federation' package
+ * with the given options.
+ *
+ * Depending on whether the `taskId` is defined or not, the task may be dependent on the completion of another task, this
+ * behavior is determined by the `taskId` parameter in the `addTask` method.
+ * If `taskId` is not defined, it invokes `addTask` without dependencies. If it is defined, the newly created task
+ * is dependent on the task with id `taskId`.
+ *
+ * After the task is added, it logs a message through the `SchematicContext` logger stating that the '@angular-architects/module-federation'
+ * package is being configured with the 'ng-add' schematic.
+ */
 function addModuleFederated(_options: Schema): Rule {
     return (tree: Tree, _context: SchematicContext) => {
         const mfDepName = '@angular-architects/module-federation';
@@ -106,7 +177,25 @@ function addModuleFederated(_options: Schema): Rule {
     }
 }
 
-function updateWebpackConfig(_options: Schema): Rule{
+/**
+ * The `updateWebpackConfig` function accepts a parameter `_options` of `Schema` type and returns a rule of the `Rule` type. 
+ * It defines the schematic context and the task to update the Webpack configuration of the Angular
+ * project within the schematic workflow.
+ *
+ * It configures the RunSchematicTask using constant string parameters `mfPlusDepName` and `mfDepPlusNameSchematicName`, as well
+ * as `options`, which is derived from `_options`. Specifically, `options` would contain properties `type`, `project`, and `port`
+ * inset by `_options.type`, `_options.project`, and `Number(_options.port)` respectively.
+ *
+ * Depending on whether the `webpackTaskId` is defined, the method `addTask` of the `_context` either includes or excludes
+ * a dependency on `webpackTaskId`. If `webpackTaskId` is undefined, the function executes `addTask` simply with task parameters, 
+ * but if `webpackTaskId` is defined, it executes `addTask` with task parameters and an array consisting `[webpackTaskId]`.
+ *
+ * After adding the task, it logs a string using `_context.logger.info` informing of the configuration status involving 
+ * named constants `mfPlusDepName`and `mfDepPlusNameSchematicName`.
+ *
+ * Finally, this method returns `tree`, which shows the current project's file system tree after the schematic modification.
+ */
+function updateWebpackConfig(_options: Schema): Rule {
     return (tree: Tree, _context: SchematicContext) => {
         const mfPlusDepName = 'module-federated-extra-schematic';
         const mfDepPlusNameSchematicName = 'ng-update-webpack';
@@ -125,6 +214,19 @@ function updateWebpackConfig(_options: Schema): Rule{
     }
 }
 
+/**
+ * The `installDependencyPackage` function creates a rule to install a given npm dependency if it's not already installed.
+ * 
+ * @param {string} depName - The name of the npm dependency to be installed.
+ * 
+ * @returns {Rule} - This function returns a rule from the '@angular-devkit/schematics' package. This rule is a function 
+ * that takes a Tree and a SchematicContext as parameters, checks if the dependency is already installed by examining the 
+ * package.json file of the project, installs the dependency if it is not installed already, and finally returns the modified tree.
+ *
+ * The function uses TypeScript AST to parse and traverse the 'package.json' source file and search for the specified dependency.
+ * In the case the dependency is not found, a new NodePackageInstallTask is added to the SchematicContext using the options 
+ * created from the name of the dependency package (depName), thereby installing the package as a new task.
+ */
 function installDependencyPackage(depName: string): Rule {
     return (tree: Tree, _context: SchematicContext) => {
         const packageJsonPath = '/package.json';
@@ -132,12 +234,12 @@ function installDependencyPackage(depName: string): Rule {
         let depInstalled = false;
 
         packageJson.forEachChild(child => {
-            if(child.kind === ts.SyntaxKind.ExpressionStatement){
+            if (child.kind === ts.SyntaxKind.ExpressionStatement) {
                 child.forEachChild(objectLiteral => {
                     objectLiteral.forEachChild(property => {
-                        if(property.getFullText().includes('dependencies')){
+                        if (property.getFullText().includes('dependencies')) {
                             property.forEachChild(dependency => {
-                                if(dependency.getFullText().includes(depName)){
+                                if (dependency.getFullText().includes(depName)) {
                                     _context.logger.info(`${depName} already installed`);
                                     depInstalled = true;
                                 }
@@ -148,7 +250,7 @@ function installDependencyPackage(depName: string): Rule {
             }
         })
 
-        if (!depInstalled){
+        if (!depInstalled) {
             const options = <NodePackageTaskOptions>{
                 packageName: depName
             };
@@ -160,9 +262,29 @@ function installDependencyPackage(depName: string): Rule {
     }
 }
 
+/**
+ * `getAsSourceFile` is a utility function which reads the file at the given path from the given Tree object,
+ * and returns it as a TypeScript Source File.
+ * 
+ * This function takes two parameters, `tree` and `path`. `tree` is the Tree object representing the current state of the
+ * filesystem, and `path` is the relative path to the file from the workspace.
+ * 
+ * This function first reads the file from the tree using the `tree.read(path)` method. If no such file exists, an instance of
+ * SchematicsException is thrown. If the file does exist, its content is converted into a string format and supplied to the
+ * `ts.createSourceFile` method from the TypeScript API along with the original path, the ScriptTarget.Latest enumeration value
+ * specifying the latest ECMAScript version as target, and a `true` value indicating the creation of a SourceFile object with
+ * setParentNodes to be true.
+ * 
+ * @param {Tree} tree - The tree from which the file is to be read.
+ * @param {string} path - The path to the file that is to be read from the tree.
+ * 
+ * @throws {SchematicsException} If the `path` does not exist in the given `tree`, then a `SchematicsException` is thrown.
+ *
+ * @returns {SourceFile} - Returns a SourceFile object from the TypeScript API, representing the file read from the tree.
+ */
 function getAsSourceFile(tree: Tree, path: string): ts.SourceFile {
     const file = tree.read(path);
-    if(!file){
+    if (!file) {
         throw new SchematicsException(`${path} file not found`);
     }
     return ts.createSourceFile(
--- src/ng-update-webpack/index.ts
+++ src/ng-update-webpack/index.ts
@@ -2,6 +2,14 @@ import {chain, Rule, SchematicContext, SchematicsException, Tree} from '@angular
 import {ProjectDefinition} from "@angular-devkit/core/src/workspace";
 import {workspaces} from "@angular-devkit/core";
 
+/**
+ * `ngUpdateWebpack` is a function that returns a Rule. The function takes in `_options` of any type and defines a rule that
+ * updates the Webpack configuration of an Angular project based on the provided options. The function uses Angular Devkit Schematics
+ * for creating, modifying, or deleting files in the workspace.
+ * 
+ * @param _options Any type indicating the options to be used in the function. Options should contain 'project' property.
+ * @returns Rule The created rule that updates the project's Webpack configuration.
+ */
 export function ngUpdateWebpack(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     const workspace = getWorkspace(_options, tree);
@@ -15,6 +23,25 @@ export function ngUpdateWebpack(_options: any): Rule {
   };
 }
 
+/**
+ * Function `getWorkspace` reads the workspace configuration of an Angular project.
+ * 
+ * It takes in two parameters: `_options` and `tree`.
+ * - `_options` is an object of type Schema, defining project-specific properties.
+ * - `tree` is an object of type Tree, defining the Angular workspace tree.
+ * 
+ * The function reads the './angular.json' file from the workspace tree.
+ * 
+ * If it doesn't find the angular.json,
+ * it throws a `SchematicsException` with a message 'angular.json file not found'.
+ * 
+ * If the file is found, the function parses the JSON file into a JavaScript object and returns it.
+ * 
+ * @param _options Schema object representing project-specific properties.
+ * @param tree Tree object representing the Angular workspace tree.
+ * @returns workspaces.WorkspaceDefinition The parsed JavaScript object from the angular.json file.
+ * @throws SchematicsException If the angular.json file is not found.
+ */
 function getWorkspace(_options: Schema, tree: Tree): workspaces.WorkspaceDefinition {
   const workspace = tree.read('./angular.json');
   if(!workspace) {
@@ -23,6 +50,21 @@ function getWorkspace(_options: Schema, tree: Tree): workspaces.WorkspaceDefinit
   return JSON.parse(workspace.toString());
 }
 
+/**
+ * The `updateWebpackConfigFile` function updates the Webpack configuration for a given project. This function takes in three parameters:
+ * `tree`, `_options`, and `project`, and returns a `Rule` which can be applied to modify the project's Tree.
+ *
+ * @param tree - `Tree` object from @angular-devkit/schematics which represents the current state of the workspace.
+ * @param _options - `Schema` object that includes project-specific properties.
+ * @param project - `ProjectDefinition | undefined` which determines the project for which the webpack configuration file needs to be updated.
+ *
+ * If the `project` parameter is not provided, it throws a SchematicsException.
+ * If the webpack configuration file is present, it reads the file, modifies its contents according to the type in `_options`, and overwrites the file.
+ * In case the configuration file does not exist, an error message is logged.
+ *
+ * @returns `Rule` - A function that returns an updated `Tree` object.
+ * @throws SchematicsException - Throws an exception if project is not found, or the type in _options is not supported.
+ */
 function updateWebpackConfigFile(tree: Tree, _options: Schema, project: undefined | ProjectDefinition): Rule{
   return (tree: Tree, _context: SchematicContext) => {
     if(!project){
@@ -61,10 +103,37 @@ function updateWebpackConfigFile(tree: Tree, _options: Schema, project: undefine
   }
 }
 
+/**
+ * `setRemoteWebpackConfig` is a function that manipulates the remote Webpack configuration in the provided string (`fileContent`).
+ *
+ * This function currently does not perform any specific manipulations but returns the provided string as it is.
+ * TODO: Implement logic for transforming Webpack configuration for remote set up.
+ *
+ * @param fileContent string representing the content of the webpack.config.js file.
+ * @returns string The transformed webpack configuration content string.
+ */
+
 function setRemoteWebpackConfig(fileContent: string): string{
 //TODO maybe any specific thing for remote
   return fileContent;
 }
+
+/**
+ * `setHostWebpackConfig` is a function that modifies the host Webpack configuration according to specific requirements. 
+ *
+ * This function takes in a string, `fileContent`, which represents the content of the webpack.config.js file and 
+ * manipulates some parts of it, primarily adjusting settings related to sharing modules. 
+ *
+ * The modifications done by this function involve the replacement of:
+ * 1. Existing 'remotes' properties with an empty object (remotes: {}).
+ * 2. 'shareAll' keyword (if any) with 'share'.
+ * 3. Existing 'shared' properties with predefined share settings for certain @angular modules (@angular/core, 
+ *    @angular/common, @angular/common/http, @angular/router). 
+ *   The shared configurations include setting singleton, strict version, and required version to 'auto'.
+ *
+ * @param fileContent A string type parameter representing the content of the webpack.config.js file.
+ * @returns string The transformed content of the webpack.config.js file.
+ */
 
 function setHostWebpackConfig(fileContent: string): string{
   let newContent =  fileContent.replace(/remotes\s*:\s*\{(?:[^{}]|\{[^{}]*\})*\}/, 'remotes: {}');
@@ -95,6 +164,22 @@ function setHostWebpackConfig(fileContent: string): string{
   return newContent;
 }
 
+/**
+ * `setCommonWebpackConfig` is a utility function that modifies the received Webpack configuration content. It operates by replacing the use of 
+ * 'shareAll' keyword with 'share' and setting predefined share settings for certain @angular modules.
+ *
+ * @param fileContent A string which represents the original content of the webpack.config.js file that needs to be updated.
+ * @returns string The modified content of the webpack.config.js file with replaced module sharing settings.
+ *
+ * This function is designed to ensure compatibility with Microfrontend architecture by enforcing specific sharing of Angular modules across 
+ * different frontend applications.
+ *
+ * The Angular modules that have their sharing settings manipulated include '@angular/core', '@angular/common', '@angular/common/http', 
+ * and '@angular/router'. For these modules, the properties 'singleton', 'strictVersion', and 'requiredVersion' are set to 'true', 
+ * 'true', and 'auto' respectively.
+ * 
+ * This optimizes the use of common Angular modules across different frontend applications reducing redundancy and improving performance.
+*/
 function setCommonWebpackConfig(fileContent: string): string{
   let newContent =  fileContent.replace(/shareAll/, 'share');
   newContent =  newContent.replace(/shared\s*:\s*\{(?:[^{}]|\{[^{}]*\})*\}/,
